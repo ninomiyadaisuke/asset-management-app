62ac3e6afb021e77b1a3b91b7ea305e1d6c8a2ba
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-05-09 10:47:12 +0900

    create Loading component

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { Loading } from '../components/atoms/Utilities';
 
 const Sample: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Loading />
+    </div>
+  );
 };
 
 export default Sample;
