c9056124c181837cde2b5bbfae36e7b59afc3214
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-05-10 14:15:13 +0900

    create Pie Chart

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { PieChart } from '../components/atoms/Chart';
+import { data } from '../lib/data';
 
 const Sample: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <PieChart data={data} />
+    </div>
+  );
 };
 
 export default Sample;
