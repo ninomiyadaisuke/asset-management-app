5caef902d6130bc58443f2aa240a2d2213597d73
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-05-12 15:15:59 +0900

    create Layout component

--- src/components/atoms/Layout/Footer.tsx
+++ src/components/atoms/Layout/Footer.tsx
@@ -5,7 +5,7 @@ import styles from '../../../styles/components/atoms/footer.module.scss';
 const Footer: FC = () => {
   return (
     <div className={styles.footer}>
-      <small className={styles.footer}>&copy;Daisuke Ninomiomiya 2022</small>
+      <small className={styles.footer}>&copy;Daisuke Ninomiya 2022</small>
     </div>
   );
 };
