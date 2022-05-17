d093deb4fa880acd9b7cfeb5aa03db650f394bb3
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-05-17 14:34:01 +0900

    create AssetsTemplate

--- src/components/template/TotalAssetsTemplate.tsx
+++ src/components/template/TotalAssetsTemplate.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react';
 import { AssetCardList } from '../organisms';
+import { SwitchChart } from '../organisms';
+import { Spacer } from '../atoms/Utilities';
+import styles from '../../styles/components/templates/total_assets.module.scss';
 
 const TotalAssets: FC = () => {
   return (
-    <div>
-      <AssetCardList />
-    </div>
+    <>
+      <Spacer size={'md'} />
+      <div className={styles.total}>
+        <div className={styles.total__chart}>
+          <SwitchChart />
+        </div>
+        <div className={styles.total__list}>
+          <AssetCardList />
+        </div>
+      </div>
+    </>
   );
 };
 
