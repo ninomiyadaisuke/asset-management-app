205c5e15af64852465c4c3939c380db48ff7357a
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-05-17 13:42:39 +0900

    initializaition Sample

--- src/pages/sample.tsx
+++ src/pages/sample.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { NextPage } from 'next';
 import { Layout } from '../components/Layout';
-import { SwitchChart } from '../components/organisms';
 
 const Sample: NextPage = () => {
   return (
     <Layout title="sample" description="sample">
-      <SwitchChart />
+      sample
     </Layout>
   );
 };
